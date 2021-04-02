7fb8f42fd4d02216d68b67d5788b9261a07816cd
Terminator-J <34873300+Terminator-J@users.noreply.github.com>
2021-04-02 08:58:29 -0700

    b4s4: Use clang 12 to compile kernel
    Change-Id: I5d66e7276d27552fc11a441c8dddc9cd03891910

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -22,6 +22,7 @@ TARGET_KERNEL_ADDITIONAL_FLAGS := \
     MKDTIMG=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/mkdtimg
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := 7211189
 TARGET_KERNEL_CONFIG := bonito_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/abc-bluecross
 TARGET_NEEDS_DTBOIMAGE := true
