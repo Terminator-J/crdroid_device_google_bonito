LineageOS/android_device_google_bonito

fixup! b4s4: RQ2A.210305.006
Change-Id: I7252685a069497fe7f5e5c9f9d36c13cc4a5e0a0

lineage_bonito.mk
@@ -19,6 +19,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bonito \
     PRIVATE_BUILD_DESC="bonito-user 11 RQ2A.210305.006 7119741 release-keys"
 
-BUILD_FINGERPRINT := google/sargo/sargo:11/RQ2A.210305.006/7119741:user/release-keys
+BUILD_FINGERPRINT := google/bonito/bonit:11/RQ2A.210305.006/7119741:user/release-keys
 
 $(call inherit-product-if-exists, vendor/google/bonito/bonito-vendor.mk)

Terminator-J/crdroid_device_google_bonito

lineage_bonito.mk
@@ -19,6 +19,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bonito \
     PRIVATE_BUILD_DESC="bonito-user 11 RQ2A.210305.006 7119741 release-keys"
 
-BUILD_FINGERPRINT := google/sargo/sargo:11/RQ2A.210305.006/7119741:user/release-keys
+BUILD_FINGERPRINT := google/bonito/bonito:11/RQ2A.210305.006/7119741:user/release-keys
 
 $(call inherit-product-if-exists, vendor/google/bonito/bonito-vendor.mk)